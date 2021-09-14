8a4d33a69d59414e5beb792cae62db6ae8a51fb3
TCM <tylermckay10@gmail.com>
2021-09-14 12:39:43 -0300

    initial implementation of conveyors

---
+++ Assets/Conveyors/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace ProcessControl.Conveyors
+{
+    public class Node : MonoBehaviour
+    {
+        public Node previous, next;
+        
+        private Vector3 position => transform.position;
+
+        private void OnDrawGizmos()
+        {
+            if (next)
+            {
+                Gizmos.color = Color.blue;
+                Gizmos.DrawLine(position, next.position);
+            }
+        }
+    }
+}
