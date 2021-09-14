76fb62fcfb750c069b1e1b8d23aa97d7430d9276
TCM <tylermckay10@gmail.com>
2021-09-14 12:39:08 -0300

    start on top down camera

--- Assets/TopDownCamera.cs
+++ Assets/TopDownCamera.cs
@@ -1,85 +1,89 @@
 using System;
 using UnityEngine;
 
-public class TopDownCamera : MonoBehaviour
+
+namespace ProcessControl.Control
 {
-    public float speed;
-    public float dragSpeed = 2;
-    public float acceleration;
-
-    public bool dragging;
-
-    private Vector3 mousePosition;
-    private Vector3 dragOrigin;
-    private Vector3 mouseInput;
-    private Vector3 cameraOrigin;
-    private Vector3 movementInput;
-    private Vector3 currentPosition;
-    private readonly Vector3 cameraOffset = new Vector3(0, 0, -10);
-
-    new private Camera camera;
-
-    private void Awake()
+    public class TopDownCamera : MonoBehaviour
     {
-        camera = GetComponent<Camera>();
-        camera.transform.position = cameraOffset;
-    }
+        public float speed;
+        public float dragSpeed = 2;
+        public float acceleration;
 
-    private void Update()
-    {
+        public bool dragging;
 
-        if (Input.GetMouseButtonDown(2))
+        private Vector3 mousePosition;
+        private Vector3 dragOrigin;
+        private Vector3 mouseInput;
+        private Vector3 cameraOrigin;
+        private Vector3 movementInput;
+        private Vector3 currentPosition;
+        private readonly Vector3 cameraOffset = new Vector3(0, 0, -10);
+
+        new private Camera camera;
+
+        private void Awake()
         {
-            dragging = true;
-            dragOrigin = camera.ScreenToWorldPoint(Input.mousePosition);
-            dragOrigin.z = 0f;
+            camera = GetComponent<Camera>();
+            camera.transform.position = cameraOffset;
+        }
+
+        private void Update()
+        {
+
+            if (Input.GetMouseButtonDown(2))
+            {
+                dragging = true;
+                dragOrigin = camera.ScreenToWorldPoint(Input.mousePosition);
+                dragOrigin.z = 0f;
+                
+                cameraOrigin = transform.position;
+            }
             
-            cameraOrigin = transform.position;
+            if (Input.GetMouseButtonUp(2)) dragging = false;
+
+            mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0f;
+
+            currentPosition = transform.position;
+
+            mouseInput.x = Input.GetAxisRaw("Mouse X");
+            mouseInput.y = Input.GetAxisRaw("Mouse Y");
+            
+            movementInput.x = Input.GetAxisRaw("Horizontal");
+            movementInput.y = Input.GetAxisRaw("Vertical");
+            movementInput.z = 0f;
+            movementInput.Normalize();
+
+            movementInput *= Time.deltaTime;
         }
-        
-        if (Input.GetMouseButtonUp(2)) dragging = false;
 
-        mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = 0f;
-
-        currentPosition = transform.position;
-
-        mouseInput.x = Input.GetAxisRaw("Mouse X");
-        mouseInput.y = Input.GetAxisRaw("Mouse Y");
-        
-        movementInput.x = Input.GetAxisRaw("Horizontal");
-        movementInput.y = Input.GetAxisRaw("Vertical");
-        movementInput.z = 0f;
-        movementInput.Normalize();
-
-        movementInput *= Time.deltaTime;
-    }
-
-    private void LateUpdate()
-    {
-        if (dragging)
+        private void LateUpdate()
         {
-            var dragOffset = mousePosition - dragOrigin;
-            transform.position -= mouseInput * (dragSpeed * Time.deltaTime);
+            if (dragging)
+            {
+                var dragOffset = mousePosition - dragOrigin;
+                transform.position -= mouseInput * (dragSpeed * Time.deltaTime);
+            }
+            else
+            {
+                var desiredPosition = currentPosition + movementInput * speed;
+                transform.position = Vector3.MoveTowards(currentPosition, desiredPosition, acceleration);
+            }
         }
-        else
+
+        private void OnDrawGizmos()
         {
-            var desiredPosition = currentPosition + movementInput * speed;
-            transform.position = Vector3.MoveTowards(currentPosition, desiredPosition, acceleration);
+            if (!Application.isPlaying) return;
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(dragOrigin, 0.2f);
+            
+            Gizmos.color = (dragging) ? Color.red : Color.green;
+            Gizmos.DrawLine(dragOrigin, mousePosition);
+            
+            Gizmos.color = Color.green;
+            Gizmos.DrawSphere(mousePosition, 0.2f);
         }
-    }
-
-    private void OnDrawGizmos()
-    {
-        if (!Application.isPlaying) return;
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(dragOrigin, 0.2f);
-        
-        Gizmos.color = (dragging) ? Color.red : Color.green;
-        Gizmos.DrawLine(dragOrigin, mousePosition);
-        
-        Gizmos.color = Color.green;
-        Gizmos.DrawSphere(mousePosition, 0.2f);
     }
 }
