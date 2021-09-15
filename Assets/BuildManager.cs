4ca17d518c1ae82c808e56805507e5359c96150a
tcm <tylermckay10@gmail.com>
2021-09-15 00:52:15 -0300

    can place nodes, will repurpose nodes, delete nodes

--- Assets/BuildManager.cs
+++ Assets/BuildManager.cs
@@ -1,18 +1,24 @@
 using System;
+using ProcessControl.Conveyors;
 using ProcessControl.Tools;
+using ProcessControl.Terrain;
 using UnityEngine;
+using UnityEngine.EventSystems;
+
 
 namespace ProcessControl.Building
 {
     public class BuildManager : MonoBehaviour
     {
-        public MonoBehaviour currentBuildItem;
+        public Node currentBuildItem;
 
         private bool buildMode;
-        private Camera camera;
+        new private Camera camera;
         
         public static Action<bool> OnBuildModeChanged;
-        public static Action<MonoBehaviour> SetBuildItem;
+        public static Action<Node> SetBuildItem;
+
+        private Node firstNode, secondNode;
 
         private void Awake()
         {
@@ -23,26 +29,69 @@ namespace ProcessControl.Building
 
         private void Update()
         {
+            
+            
             if (Input.GetKeyDown(KeyCode.B))
             {
                 buildMode = !buildMode;
                 OnBuildModeChanged?.Invoke(buildMode);
             }
             
-            if (!buildMode || !currentBuildItem) return;
+            if (!buildMode || !currentBuildItem || EventSystem.current.IsPointerOverGameObject()) return;
 
             if (Input.GetKeyDown(KeyCode.Mouse0))
             {
-                var mousePosition = camera.ViewportToWorldPoint(Input.mousePosition);
-                mousePosition.z = 0f;
-                
-                Factory.Spawn(currentBuildItem, mousePosition);
+                var firstCell = ProceduralGrid.GetCellPosition(camera.MouseWorldPosition2D());
+                if (firstCell is null) Debug.Log("NO CELL FOUND!");
+                else
+                {
+                    firstNode = (firstCell.occupied) ? firstCell.node : Factory.Spawn(currentBuildItem, firstCell.center);
+                    firstCell.node = firstNode;
+                }
             }
+
+            if (Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                var secondCell = ProceduralGrid.GetCellPosition(camera.MouseWorldPosition2D());
+                if (secondCell is null) Debug.Log("NO CELL FOUND!");
+                else
+                {
+                    secondNode = (secondCell.occupied) ? secondCell.node : Factory.Spawn(currentBuildItem, secondCell.center);
+                    secondCell.node = secondNode;
+                    
+                    firstNode.next = secondNode;
+                    secondNode.previous = firstNode;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                var cell = ProceduralGrid.GetCellPosition(camera.MouseWorldPosition2D());
+                if (cell is null) Debug.Log("NO CELL FOUND!");
+                else
+                if (cell.occupied)
+                {
+                    Destroy(cell.node.gameObject);
+                    cell.node = null;
+                }
+            }
+
         }
 
-        private void OnSetBuildItem(MonoBehaviour newBuildItem)
+        private void OnSetBuildItem(Node newBuildItem)
         {
             currentBuildItem = newBuildItem;
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (buildMode)
+            {
+                Gizmos.color = Color.white;
+                var cell = ProceduralGrid.GetCellPosition(camera.MouseWorldPosition2D());
+                if (cell is null) return;
+                Gizmos.DrawWireCube(cell.center, Vector3.one);
+            }
         }
     }
 }
