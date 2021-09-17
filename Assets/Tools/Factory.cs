0f2e3d26e6a99d335f5de41d8ecf0f181ec37343
tcm <tylermckay10@gmail.com>
2021-09-16 23:02:55 -0300

    spawn prefab in scene

--- Assets/Tools/Factory.cs
+++ Assets/Tools/Factory.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 namespace ProcessControl.Tools
@@ -6,11 +9,23 @@ namespace ProcessControl.Tools
     [CreateAssetMenu(fileName = "Factory", menuName = "Tools/Factory")]
     public class Factory : ScriptableObject
     {
+        //> CREATE AND INSTANCE ON PREFAB
+        public static T Spawn<T>(string sceneName, T prefab, Vector3 position) where T : MonoBehaviour
+        {
+            var scene = SceneManager.GetSceneByName(sceneName);
+            if (!scene.isLoaded) scene = SceneManager.CreateScene(sceneName);
+
+            T instance = Instantiate(prefab, position, quaternion.identity);
+            instance.name = prefab.name;
+            SceneManager.MoveGameObjectToScene(instance.gameObject, scene);
+            return instance;
+        }
+
         public static T Spawn<T>(T prefab, Vector3 position, Quaternion rotation) where T : MonoBehaviour
             => Instantiate(prefab, position, rotation);
 
-        public static T Spawn<T>(T prefab, Vector3 position) where T : MonoBehaviour
-            => Spawn(prefab, position, Quaternion.identity);
+        // public static T Spawn<T>(T prefab, Vector3 position) where T : MonoBehaviour
+        //     => Spawn(prefab, position, Quaternion.identity);
         
         public static T Spawn<T>(T prefab) where T : MonoBehaviour
             => Spawn(prefab, Vector3.zero, Quaternion.identity);
