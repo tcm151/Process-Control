c7a94a950b8d95e6faaa191fe21b25b9a3e46677
tcm <tylermckay10@gmail.com>
2021-09-16 23:02:15 -0300

    Resource is it's own class

---
+++ Assets/Machines/Resource.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace ProcessControl.Conveyors
+{
+    public class Resource : MonoBehaviour
+    {
+        [Serializable] public class Data
+        {
+            public Color color = Color.grey;
+            
+            public Vector3 position;
+            public Vector2Int coordinates;
+        }
+
+        public Data data;
+        
+        private void Awake()
+        {
+            data.position = transform.position;
+        }
+
+        public void SetCoordinates(Vector2Int newCoordinates)
+        {
+            data.coordinates = newCoordinates;
+        }
+
+        public void OnDrawGizmos()
+        {
+            Gizmos.color = data.color;
+            Gizmos.DrawCube(data.position, 0.25f * Vector3.one);
+        }
+    }
+}
