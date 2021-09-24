def7078c19a427ba99b5dcf52dcc28258f26539a
tcm <tylermckay10@gmail.com>
2021-09-24 01:23:56 -0300

    starting on terrain generation

---
+++ Assets/Procedural/Noise/Noise.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+
+namespace TCM.NoiseGeneration
+{
+    public static class Noise
+    {
+        //> NOISE TYPES
+        public enum Type { Simple, Rigid };
+
+        //> NOISE LAYER SETTINGS
+        [System.Serializable] public class Layer
+        {
+            public bool enabled = true;
+            public bool useMask = true;
+
+            public Type noiseType;
+            public SimplexNoise generator = new SimplexNoise();
+
+            [Range( 1,  8)] public int octaves = 1;
+            [Range( 0, 10)] public float strength = 1f;
+            [Range( 0,  5)] public float baseRoughness = 1f;
+            [Range( 0, 15)] public float roughness = 2f;
+            [Range( 0,  5)] public float persistence = 0.5f;
+            [Range(-5,  5)] public float localZero = 0f;
+            public Vector3 offset = Vector3.zero;
+        }
+
+        //> GET A NOISE VALUE FROM A VECTOR3
+        public static float GenerateValue(Layer noise, Vector3 vector3)
+        {
+            float generatedValue = 0f;
+            float roughness = noise.baseRoughness;
+            float amplitude = 1;
+            float weight = 1;
+
+            switch (noise.noiseType)
+            {
+                //> SIMPLE NOISE FILTER
+                case Type.Simple:
+                {
+                    for (int i = 0; i < noise.octaves; i++)
+                    {
+                        float value = noise.generator.Generate(vector3 * roughness + noise.offset);
+                        generatedValue += (value + 1) / 2 * amplitude;
+                        roughness *= noise.roughness;
+                        amplitude *= noise.persistence;
+                    } break;
+                }
+
+                //> MOUNTAINOUS NOISE FILTER
+                case Type.Rigid:
+                {
+                    for (int i = 0; i < noise.octaves; i++)
+                    {
+                        float value = 1 - Mathf.Abs(noise.generator.Generate(vector3 * roughness + noise.offset));
+                        value *= value * weight;
+                        weight = value;
+                        generatedValue += value * amplitude;
+                        roughness *= noise.roughness;
+                        amplitude *= noise.persistence;
+                    } break;
+                }
+            }
+
+            generatedValue = Mathf.Max(0, generatedValue - noise.localZero);
+            return generatedValue * noise.strength;
+        }
+    }
+}
---
+++ Assets/Procedural/ProceduralGrid.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using ProcessControl.Tools;
+using ProcessControl.Graphs;
+using ProcessControl.Industry.Resources;
+using TCM.NoiseGeneration;
+
+#pragma warning disable 108,114
+
+
+namespace ProcessControl.Procedural
+{
+    [RequireComponent(typeof(Grid))]
+    public class ProceduralGrid : MonoBehaviour
+    {
+        [Serializable] public class Cell
+        {
+            public void ResetNode() => node = null;
+            
+            public Node node;
+            public bool occupied => node is { };
+
+            public float value;
+            
+            public Vector2Int coordinates;
+            public Vector3 center;
+
+            public List<(int, Resource.Type)> resourceDeposit = new List<(int, Resource.Type)>();
+        }
+
+        public Vector2Int dimensions;
+        public Tilemap tilemap;
+        public List<TileBase> tiles;
+        public List<Cell> cells;
+
+        private Noise.Layer noiseSettings;
+
+        private RectInt gridRect;
+
+        private Camera camera;
+
+        public static Func<Cell> GetCellUnderMouse;
+        public static Func<Vector2Int, Cell> GetCellCoords;
+        public static Func<Vector3, Cell> GetCellPosition;
+        
+        private void Awake()
+        {
+            camera = Camera.main;
+            
+            //- register events
+            GetCellCoords += OnGetCellCoords;
+            GetCellPosition += OnGetCellPosition;
+            GetCellUnderMouse += OnGetCellUnderMouse;
+            
+            //! temp
+            gridRect.height = dimensions.y;
+            gridRect.width = dimensions.x;
+            gridRect.x = -dimensions.x / 2;
+            gridRect.y = -dimensions.y / 2;
+
+            cells = new List<Cell>();
+            tilemap = GetComponentInChildren<Tilemap>();
+            
+            for (int y = gridRect.y; y < -gridRect.y; y++) {
+                for (int x = gridRect.x; x < -gridRect.x; x++)
+                {
+                    // Debug.Log($"Adding cell ({x},{y})");
+                    
+                    cells.Add(new Cell
+                    {
+                        center = new Vector3(x + 0.5f, y + 0.5f),
+                        coordinates = new Vector2Int(x, y),
+                    });
+                }
+            }
+
+            
+            cells.ForEach(c =>
+            {
+                var noise = Noise.GenerateValue(noiseSettings, new Vector3(c.coordinates.x, c.coordinates.y, 0f));
+                c.value = noise;
+                tilemap.SetTile(new Vector3Int(c.coordinates.x, c.coordinates.y, 0), tiles[0]);
+            });
+        }
+        
+        //> GET CELLS 
+        private Cell OnGetCellUnderMouse()
+        {
+            var mousePosition = camera.MouseWorldPosition2D();
+            return OnGetCellPosition(mousePosition);
+        }
+        private Cell OnGetCellCoords(Vector2Int coordinates)
+        {
+            var cell = cells.FirstOrDefault(c => c.coordinates == coordinates);
+            return cell;
+        }
+        private Cell OnGetCellPosition(Vector3 worldPosition)
+        {
+            var coords = new Vector2Int(worldPosition.x.FloorToInt(), worldPosition.y.FloorToInt());
+            // Debug.Log(coords);
+            return OnGetCellCoords(coords);
+        }
+    }
+}
