f4262d19004fe9debbef4419019c1e630c14366c
TCM <tylermckay10@gmail.com>
2021-11-01 12:46:27 -0300

    smelter uses coal

--- Assets/Industry/Machines/Smelter.cs
+++ Assets/Industry/Machines/Smelter.cs
@@ -1,16 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ProcessControl.Industry.Resources;
 using UnityEngine;
 
 namespace ProcessControl.Industry.Machines
 {
     public class Smelter : Machine
     {
-        // [Serializable] new public class Data
-        // {
-            public int energy;
-            public int maxEnergy;
-        // }
-        // [SerializeField] internal Data smelter;
+        [Header("Smelter")]
+        public List<Resource> acceptedFuels;
+        
+        public int energy;
+        public int maxEnergy;
         
         [Range(1, 64)] public float smeltingSpeed;
 
@@ -23,9 +25,26 @@ namespace ProcessControl.Industry.Machines
                 ticks = 0;
                 if (inputInventory.Count == 0) return;
 
-                if (energy < maxEnergy)
+                if (acceptedFuels.Any(f => inputInventory.Contains(f)))
                 {
+                    var match = acceptedFuels.FirstOrDefault(f => inputInventory.Contains(f));
+                    if (match is null)
+                    {
+                        Debug.Log("NO FUEL!");
+                        return;
+                    }
+
+                    if (energy > maxEnergy - match.energy)
+                    {
+                        Debug.Log("Already enough energy...");
+                        return;
+                    }
                     
+                    var fuel = inputInventory.Withdraw(match);
+                    Debug.Log("Withdrawing fuel..");
+                    
+                    energy += match.energy;
+                    Debug.Log($"Added {match.energy} energy...");
                 }
                 
                 Smelt();
@@ -38,6 +57,7 @@ namespace ProcessControl.Industry.Machines
             {
                 currentRecipe.inputItems.ForEach(i => inputInventory.Withdraw(i.item, i.amount));
                 currentRecipe.outputItems.ForEach(r => outputInventory.Deposit(r.item, r.amount));
+                energy -= currentRecipe.energyCost;
             }
         }
     }
