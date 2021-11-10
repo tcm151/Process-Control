a9614a500e807e7269c72a4a6172dfc865f556d1
TCM <tylermckay10@gmail.com>
2021-11-09 22:43:17 -0400

    track all jobs all the time

--- Assets/Jobs/AgentManager.cs
+++ Assets/Jobs/AgentManager.cs
@@ -3,47 +3,57 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
-using ProcessControl.Jobs;
 using ProcessControl.Tools;
 
-public class AgentManager : MonoBehaviour
+
+namespace ProcessControl.Jobs
 {
-    public static Action<Job> QueueJob;
-    public static Action<List<Job>> QueueJobs;
-
-    [SerializeField] private List<Worker> busyWorkers = new List<Worker>();
-    [SerializeField] private List<Worker> openWorkers = new List<Worker>();
-    [SerializeField] private List<Job> openJobs = new List<Job>();
-
-    private void Awake()
+    public class AgentManager : MonoBehaviour
     {
-        QueueJob += (job) => openJobs.Add(job);
-        QueueJobs += (jobList) => jobList.ForEach(j => openJobs.Add(j));
+        public static Action<Job> QueueJob;
+        public static Action<List<Job>> QueueJobs;
         
-        openWorkers = FindObjectsOfType<Worker>().ToList();
-        openWorkers.ForEach(w =>
-        {
-            w.onJobCompleted += () =>
-            {
-                busyWorkers.Remove(w);
-                openWorkers.Add(w);
-            };
-        });
-    }
-    
-    private void FixedUpdate()
-    {
-        
-        if (openJobs.Count >= 1 && openWorkers.Count >= 1)
-        {
-            var worker = openWorkers.TakeFirst();
+        [SerializeField] private List<Job> openJobs = new List<Job>();
+        [SerializeField] private List<Job> takenJobs = new List<Job>();
+        [SerializeField] private List<Job> completedJobs = new List<Job>();
+        [SerializeField] private List<Worker> openWorkers = new List<Worker>();
+        [SerializeField] private List<Worker> busyWorkers = new List<Worker>();
 
-            // worker.TakeJob(openJobs.TakeFirst());
-            var closestJob = openJobs.OrderBy(j => Vector3.Distance(worker.position, j.location)).First();
-            openJobs.Remove(closestJob);
-            worker.TakeJob(closestJob);
+        private void Awake()
+        {
+            QueueJob += (job) => openJobs.Add(job);
+            QueueJobs += (jobList) => jobList.ForEach(j => openJobs.Add(j));
             
-            busyWorkers.Add(worker);
+            openWorkers = FindObjectsOfType<Worker>().ToList();
+            openWorkers.ForEach(worker => 
+            {
+                worker.onJobCompleted += () => 
+                {
+                    openWorkers.Add(worker);
+                    busyWorkers.Remove(worker);
+
+                    var job = worker.currentJob;
+                    takenJobs.Remove(job);
+                    completedJobs.Add(job);
+                    worker.currentJob = null;
+                };
+            });
+        }
+        
+        private void FixedUpdate()
+        {
+            if (openJobs.Count >= 1 && openWorkers.Count >= 1)
+            {
+                var worker = openWorkers.TakeFirst();
+
+                var closestJob = openJobs.OrderBy(j => Vector3.Distance(worker.position, j.location)).First();
+                openJobs.Remove(closestJob);
+                worker.TakeJob(closestJob);
+                takenJobs.Add(closestJob);
+                
+                busyWorkers.Add(worker);
+            }
         }
     }
 }
+
