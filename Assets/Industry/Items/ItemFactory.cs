656690d248b226a49a462db27222eaa39d7183e2
tcm <tylermckay10@gmail.com>
2021-11-11 17:03:36 -0400

    get items by name

--- Assets/Industry/Items/ItemFactory.cs
+++ Assets/Industry/Items/ItemFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using ProcessControl.Industry;
 
@@ -9,13 +10,26 @@ namespace ProcessControl.Tools
     {
         public string sceneName = "Items";
         public Container container;
+        [SerializeField] public List<Item> itemPrefabs;
+        [SerializeField] private List<Container> spawnedContainers = new List<Container>();
 
+        public Item GetItem(string name)
+        {
+            var item = itemPrefabs.FirstOrDefault(i => i.name == name);
+            if (item is {}) return item;
+            
+            Debug.Log($"Item \"{name}\" was not found.");
+            return default;
+        }
+
+        public Part GetPart(string name)
+        {
+            var item = GetItem(name);
+            return (item is Part part) ? part : default;
+        }
+        
         private void Awake() => Instance = this;
         public static ItemFactory Instance {get; private set;}
-
-        private readonly List<Container> spawnedContainers = new List<Container>();
-        
-        
 
         public Container SpawnItem(Item item, Vector3 position)
         {
