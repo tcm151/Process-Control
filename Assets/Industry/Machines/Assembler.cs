4bb4f8747a990a50a0fd202e70ed2d5d7205e8b8
tcm <tylermckay10@gmail.com>
2021-12-04 01:29:29 -0400

    assembler

---
+++ Assets/Industry/Machines/Assembler.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+
+namespace ProcessControl.Industry.Machines
+{
+    public class Assembler : Machine
+    {
+        private bool assembling;
+        
+        virtual protected void FixedUpdate()
+        {
+            if (assembling) return;
+
+            if (currentRecipe.inputItems.TrueForAll(itemAmount => inputInventory.Contains(itemAmount)))
+            {
+                AssembleRecipe();
+            }
+        }
+        
+        private async void AssembleRecipe()
+        {
+            assembling = true;
+
+            while (ticks++ < currentRecipe.assemblyTime)
+            {
+                await Task.Yield();
+            }
+            
+            currentRecipe.outputItems.ForEach(
+                i =>
+                {
+                    outputInventory.Deposit(i);    
+                });
+
+            ticks = 0;
+            assembling = false;
+        }
+
+        public void CreateJob()
+        {
+            
+        }
+    }
+    
+}
